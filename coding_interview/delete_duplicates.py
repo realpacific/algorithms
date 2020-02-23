d6aa8b4634c72c829838660d0ce2ccf4b8f9660b
Prashant <prashantbarahi@gmail.com>
2020-02-23 22:33:48 +0545

    linked list

---
+++ coding_interview/delete_duplicates.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from shared.linked_list import Node, LinkedList
+
+
+def delete_duplicates(linkedlist):
+    current = linkedlist.node
+    while current is not None:
+        runner = current
+        while runner.next is not None:
+            if runner.next.data == current.data:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        current = current.next
+
+
+linked = LinkedList()
+linked.add(Node(1))
+linked.add(Node(2))
+linked.add(Node(2))
+linked.add(Node(2))
+linked.add(Node(3))
+linked.add(Node(4))
+linked.add(Node(5))
+linked.add(Node(1))
+linked.add(Node(6))
+linked.add(Node(7))
+delete_duplicates(linked)
+linked.print()
---
+++ coding_interview/shared/linked_list.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return f"Node({self.data}, {self.next})"
+
+    def __repr__(self):
+        return str(self)
+
+
+class LinkedList:
+    def __init__(self):
+        self.node = None
+
+    def add(self, data):
+        if not isinstance(data, Node):
+            raise TypeError()
+
+        if self.node is None:
+            self.node = data
+        else:
+            current = self.node
+            while current.next is not None:
+                current = current.next
+            current.next = data
+
+    def __iter__(self):
+        for each in self.__dict__.keys():
+            yield self.__getattribute__(each)
+
+    def print(self):
+        current = self.node
+        while(current is not None):
+            print(current.data)
+            current = current.next
