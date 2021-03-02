716b10f8af40259d1440d100d993d068cc11acde
realpacific <prashantbarahi@gmail.com>
2021-03-02 21:10:31 +0545

    Graph

--- src/algorithmdesignmanualbook/graph/Graph.kt
+++ src/algorithmdesignmanualbook/graph/Graph.kt
@@ -7,6 +7,19 @@ class Graph(private val isDirected: Boolean) {
         return vertices.containsKey(value)
     }
 
+    fun getRandomVertex(): Vertex {
+        return vertices[vertices.keys.elementAt(0)]!!
+    }
+
+
+    fun getVertex(str: String): Vertex {
+        return vertices[str]!!
+    }
+
+    fun isConnected(v1: String, v2: String): Boolean {
+        return (vertices[v1] ?: return false).isConnectTo(vertices[v2] ?: return false)
+    }
+
     fun addVertex(value: String): Vertex {
         require(!vertices.containsKey(value))
         vertices[value] = Vertex(value)
@@ -36,6 +49,11 @@ class Graph(private val isDirected: Boolean) {
         var edges = mutableSetOf<Edge>()
         var state = State.UNDISCOVERED
 
+        fun isConnectTo(v2: Vertex): Boolean {
+            val edge = Edge(this, v2)
+            return edges.contains(edge)
+        }
+
         fun addEdge(vertex: Vertex, weight: Int = 0) {
             val edge = Edge(this, vertex)
             edge.weight = weight
@@ -57,6 +75,52 @@ class Graph(private val isDirected: Boolean) {
 
         override fun toString(): String {
             return "Vertex(${endVertex.value}, weight=$weight)"
+        }
+    }
+
+    companion object {
+        fun getDefaultDirected(): Graph {
+            val graph = Graph(true)
+            val v1 = graph.addVertex("1")
+            val v2 = graph.addVertex("2")
+            val v3 = graph.addVertex("3")
+            val v4 = graph.addVertex("4")
+            val v5 = graph.addVertex("5")
+            val v6 = graph.addVertex("6")
+            val v7 = graph.addVertex("7")
+            graph.join(v2, v3)
+            graph.join(v1, v2)
+            graph.join(v1, v3)
+            graph.join(v1, v5)
+            graph.join(v5, v4)
+            graph.join(v3, v6)
+            graph.join(v4, v7)
+            return graph
+        }
+
+        /**
+         *     2---3
+         *   / |   |
+         * 1   |   4
+         * |\ |---/
+         * 6  5
+         *
+         */
+        fun getDefaultUnDirected(): Graph {
+            val graph = Graph(false)
+            val v1 = graph.addVertex("1")
+            val v2 = graph.addVertex("2")
+            val v3 = graph.addVertex("3")
+            val v4 = graph.addVertex("4")
+            val v5 = graph.addVertex("5")
+            val v6 = graph.addVertex("6")
+            graph.join(v1, v2)
+            graph.join(v2, v3)
+            graph.join(v3, v4)
+            graph.join(v4, v5)
+            graph.join(v5, v1)
+            graph.join(v1, v6)
+            return graph
         }
     }
 }
