a3e54ebabff81c405c3c0c298f359ec4204b401d
realpacific <prashantbarahi@gmail.com>
2021-08-22 00:16:56 +0545

    Knapsack and quadtree added

---
+++ src/algorithmsinanutshell/spatialtree/KnapSack01.kt
@@ -0,0 +1,114 @@
+package algorithmsinanutshell.spatialtree
+
+import utils.PrintUtils
+import kotlin.math.max
+import kotlin.test.assertTrue
+
+/**
+ * Knapsack Problem: Given total capacity [totalCapacity] of a bag,
+ * how can you select subset of items of weight [weights] with profit [profits] such that the profit is maximized?
+ *
+ * [Video](https://www.youtube.com/watch?v=8LusJS5-AGo)
+ *
+ * ```
+ * | P | w | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+ * | 1 | 1 |   |   |   |   |   |   |   |   |
+ * | 4 | 3 |   |   |   |   |   |   |   |   | <-- while here, everything below are ignored so w1 and w3 are considered
+ * | 5 | 4 |   |   |   |   |   |   |   |   |
+ * | 6 | 5 |   |   |   |   |   |   |   |   |
+ * ```
+ *
+ */
+class KnapSack01(val weights: IntArray, val profits: IntArray, val totalCapacity: Int) {
+
+    private val matrix: Array<IntArray> = Array(profits.size + 1) {
+        IntArray(totalCapacity + 1) {
+            0
+        }
+    }
+
+    fun getProfitMatrix(): Array<IntArray> {
+        for (row in 1..profits.size) {
+            for (currentCapacity in 0..totalCapacity) {
+                if (weights[row - 1] <= currentCapacity) {
+                    // Which item can fit after we choose weights[row-1]
+                    val remainingSpace = currentCapacity - weights[row - 1]
+
+                    matrix[row][currentCapacity] = max(
+                        matrix[row - 1][currentCapacity],  // Previous best profit
+                        // I select an item, its profit = values[row - 1]
+                        // now I can select any item that can fit in the remaining space
+                        (profits[row - 1]) + (matrix[row - 1][remainingSpace])
+                    )
+                } else {
+                    matrix[row][currentCapacity] = matrix[row - 1][currentCapacity]
+                }
+            }
+        }
+        return matrix
+    }
+
+    fun getSelectedWeight(): MutableList<Int> {
+        val selection = mutableListOf<Int>()
+        // Start from the bottom right corner of the matrix
+        var row = matrix.lastIndex
+        var col = matrix[0].lastIndex
+        for (i in weights.lastIndex downTo 0) {
+            compare(row, col, selection).apply {
+                row = this.first
+                col = this.second
+            }
+        }
+        println(selection)
+        return selection
+    }
+
+    private fun compare(row: Int, col: Int, selection: MutableList<Int>): Pair<Int, Int> {
+        val currentProfit = matrix[row][col]
+        val profitOneLevelUp = matrix[row - 1][col]
+
+        // Do nothing if total selected weight exceeds [totalCapacity]
+        if (selection.sum() >= totalCapacity) {
+            return row to col
+        }
+
+        // Compare current profit with profit directly above
+        // Select current weight if they differ
+        return if (currentProfit != profitOneLevelUp) {
+            // Select the weight
+            val candidateForSelection = weights[row - 1]
+            selection.add(candidateForSelection)
+            // Find remaining space after the weight is selected
+            val remainingSpace = totalCapacity - candidateForSelection
+            // Go one step above and then to remaining space available
+            row - 1 to remainingSpace
+        } else {
+            // Else go one step above and repeat
+            row - 1 to col
+        }
+    }
+}
+
+fun main() {
+    val weights = intArrayOf(1, 3, 4, 5)
+    val profits = intArrayOf(1, 4, 5, 7)
+    val totalCapacity = 7
+    val algorithm = KnapSack01(
+        weights = weights,
+        profits = profits,
+        totalCapacity = totalCapacity
+    )
+    algorithm.getProfitMatrix().map {
+        PrintUtils.print(it.toList())
+    }
+    val selectedWeight = algorithm.getSelectedWeight()
+    assertTrue {
+        selectedWeight.sum() == totalCapacity
+    }
+    assertTrue {
+        selectedWeight.map { weight ->
+            val index = weights.indexOf(weight)
+            profits[index]
+        }.sum() == 9
+    }
+}
---
+++ src/algorithmsinanutshell/spatialtree/QuadTree.kt
@@ -0,0 +1,107 @@
+package algorithmsinanutshell.spatialtree
+
+import algorithmsinanutshell.Point
+
+/**
+ * https://algs4.cs.princeton.edu/92search/QuadTree.java.html
+ *
+ * https://www.youtube.com/watch?v=jxbDYxm-pXg
+ *
+ * https://www.youtube.com/watch?v=xFcQaig5Z2A
+ */
+class QuadTree {
+
+    /**
+     * QuadNode consists of a value and 4 quadrants.
+     *
+     *          |  sw  |  se  |  nw  |  ne  |
+     */
+    inner class QuadNode(
+        val value: Int, val point: Point
+    ) {
+        var sw: QuadNode? = null
+        var se: QuadNode? = null
+        var nw: QuadNode? = null
+        var ne: QuadNode? = null
+
+        val x: Int
+            get() = point.x
+
+
+        val y: Int
+            get() = point.y
+
+        override fun toString(): String {
+            return "QuadNode($value, sw=$sw, se=$se, nw=$nw, ne=$ne)"
+        }
+    }
+
+    private var root: QuadNode? = null
+
+    fun insert(x: Int, y: Int, value: Int) {
+        val newNode = QuadNode(value, point = Point(x, y))
+        root = insert(root, newNode)
+    }
+
+    private fun insert(parent: QuadNode?, node: QuadNode): QuadNode {
+        if (parent == null) {
+            return node
+        } else if (node.x < parent.x && node.y < parent.y) {
+            parent.sw = insert(parent.sw, node)
+        } else if (node.x < parent.x && node.y > parent.y) {
+            parent.nw = insert(parent.nw, node)
+        } else if (node.x > parent.x && node.y < parent.y) {
+            parent.se = insert(parent.se, node)
+        } else if (node.x > parent.x && node.y > parent.y) {
+            parent.ne = insert(parent.ne, node)
+        }
+        return parent
+    }
+
+    override fun toString(): String {
+        return "QuadTree($root)"
+    }
+
+    /**
+     * Given a rectangle with ([xmin], [ymin]) and ([xmax], [ymax]), find the points inside it.
+     */
+    fun rangeQuery(xmin: Int, ymin: Int, xmax: Int, ymax: Int): List<QuadNode> {
+        val result = mutableListOf<QuadNode>()
+        rangeQuery(root!!, xmin, ymin, xmax, ymax, result)
+        return result
+    }
+
+    private fun rangeQuery(root: QuadNode?, xmin: Int, ymin: Int, xmax: Int, ymax: Int, result: MutableList<QuadNode>) {
+        if (root == null) {
+            return
+        }
+        if (root.x in xmin..xmax && root.y in ymin..ymax) {
+            result.add(root)
+        }
+
+        if (root.x > xmin && root.y > ymin) {
+            rangeQuery(root.sw, xmin, ymin, xmax, ymax, result)
+        }
+        if (root.x > xmin && root.y < ymax) {
+            rangeQuery(root.nw, xmin, ymin, xmax, ymax, result)
+        }
+        if (root.x < xmax && root.y > ymin) {
+            rangeQuery(root.se, xmin, ymin, xmax, ymax, result)
+        }
+        if (root.x < xmax && root.y < ymax) {
+            rangeQuery(root.ne, xmin, ymin, xmax, ymax, result)
+        }
+    }
+}
+
+fun main() {
+    val tree = QuadTree()
+    tree.insert(10, 10, 1010)
+    tree.insert(11, 11, 1111)
+    tree.insert(5, 5, 55)
+    tree.insert(3, 3, 33)
+    tree.insert(2, 2, 22)
+    tree.insert(4, 4, 43)
+    println(tree)
+    tree.rangeQuery(1, 1, 6, 6).forEach { println(it) }
+}
